6800f78758ea5cbdaad55c32c5d3e955a0771a51
gauravcrafter-lgtm <gauravcrafter@gmail.com>
2026-01-07 12:52:27 +0530

    Initial commit

---
+++ practice.js
@@ -0,0 +1,85 @@
+// let arr="my name is a gaurav";
+// let rev= arr.split("").reverse().join("");
+// console.log(rev);  
+
+// let arr=[1,4,32,3,5,,5,5,65,6]
+// let rev=arr.reverse();
+// console.log(rev);
+
+// let arr=["g","a","u","r","a","v"]
+// let rev=arr.reverse();
+// console.log(rev);
+
+// let arr=[1,1,1,2,3,,4,,4,5,,5,6,6,,6,6,7]
+// let dupliacte =[...new Set(arr)];
+// console.log(dupliacte);
+
+// let arr="gaurav pal"
+// let rev=arr.split("").reverse().join("");
+// console.log(rev);
+ 
+// let arr=[21,23,45,65,78324,2321];
+// let max=Math.max(...arr);
+// let min=Math.min(...arr);
+
+// console.log("max:",max);
+// console.log("min:",min);
+
+// let arr="gaurav bhai keedicheez";
+// let rev=arr.split("").reverse().join("");
+// if(arr==rev)
+// {
+//     console.log("palidrome");
+// }
+// else
+// {
+//     console.log("not palidorme");
+// }
+
+
+
+
+
+
+// let arr="mam";
+// let rev = arr.split("").reverse().join("");
+
+// if(arr==rev)
+// {
+//     console.log("palidrome");
+
+// }
+// else{
+//     console.log("not palidrome");
+// }
+
+
+// let arr="mam";
+// let rev=arr.split("").reverse().join("");
+// if(arr==rev)
+// {
+//     console.log("palidrome")
+// }
+// else{
+//     console.log("not palidrome")
+// }
+
+// let arr=[1,2,3,4,,5,5,6,,6,6,];
+// let rev=arr.reverse("");
+// console.log(rev)
+
+// let arr=["g","a","u","r","a","v"];
+// let rev=arr.reverse().join("");
+// console.log(rev)
+
+// let arr=[1,2,3,,3,4,,4,5,5,6,6,6,]
+// let dupliacte=[...new Set(arr)];
+// console.log(dupliacte)
+
+// let arr=[1,3,3,4,5,5,6,6,6,7]
+// let max = Math.max (...arr);
+// let min = Math.min(...arr);
+
+// console.log("max",max);
+// console.log("min",min);
+
